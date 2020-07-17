9b0ccb5305e18089f3caa87b5ce37971a756e499
Matthew <matthew.sun@ithaka.org>
2020-07-17 12:30:12 -0400

    add a functional component as example

--- src/component/CalButton/index.tsx
+++ src/component/CalButton/index.tsx
@@ -8,17 +8,23 @@ const initialState = {
 export type ICalButtonProps = { character: string, onClick: () => any }
 type ICalButtonState = typeof initialState
 
-class CalButton extends React.Component<ICalButtonProps, ICalButtonState>{
-    constructor(props: ICalButtonProps) {
-        super(props)
-        this.state = initialState
-    }
+// class CalButton extends React.Component<ICalButtonProps, ICalButtonState>{
+//     constructor(props: ICalButtonProps) {
+//         super(props)
+//         this.state = initialState
+//     }
 
-    render() {
-        return <button className='calbutton' onClick={this.props.onClick}>
-            <div>{this.props.character}</div>
-        </button >
-    }
+//     render() {
+//         return <button className='calbutton' onClick={this.props.onClick}>
+//             <div>{this.props.character}</div>
+//         </button >
+//     }
+// }
+
+const CalButton = (props: ICalButtonProps) => {
+    return <button className='calbutton' onClick={props.onClick}>
+        <div>{props.character}</div>
+    </button >
 }
 
 export default CalButton
